cd1b9e2674b2a942c9e30c5ad95e490dacfa87ac
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2022-10-01 20:48:47 -0300

    Add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
 	import ImageSearchForm from '$lib/ImageSearchForm.svelte';
 </script>
 
+<svelte:head>
+	<title>Starsense - Making the universe available at your fingertips</title>
+</svelte:head>
+
 <div class="flex flex-col grow space-y-4">
 	<div class="p-4 space-y-4">
 		<ImageSearchForm />
