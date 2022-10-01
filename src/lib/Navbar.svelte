bcbe66f2eab08b13ec5668c1b7d77c75990635fe
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2022-10-01 19:54:17 -0300

    Add navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,8 @@
+<div class="navbar bg-base-200">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost space-x-2">
+			<img src="/logo.svg" alt="Starsense logo" width="40" height="40" />
+			<span>Starsense</span>
+		</a>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/Navbar.svelte';
 </script>
 
-<slot />
+<div class="flex flex-col h-screen">
+	<Navbar />
+	<main class="container mx-auto p-4">
+		<slot />
+	</main>
+</div>
