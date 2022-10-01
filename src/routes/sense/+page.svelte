fe17f4928194b23922835ee8ea9eec3dd9ddcdce
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2022-10-01 20:42:16 -0300

    Fix issue when reloading sense page

--- src/routes/sense/+page.svelte
+++ src/routes/sense/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 	import Sensor from '$lib/Sensor.svelte';
+	import { selectedImageItem } from '$lib/stores';
+
+	onMount(async () => {
+		if ($selectedImageItem === null) {
+			await goto('/');
+		}
+	});
 </script>
 
 <Sensor />
