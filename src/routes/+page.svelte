e7fa6307ccef466cc3714c3dd93a53dc264f721d
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2022-10-02 13:56:10 -0300

    Add missing title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,6 @@
 	import ImageSearchForm from '$lib/ImageSearchForm.svelte';
 </script>
 
-<svelte:head>
-	<title>Starsense - The universe at your fingertips</title>
-</svelte:head>
-
 <div class="flex flex-col grow space-y-4">
 	<div class="p-4 space-y-4">
 		<ImageSearchForm />
